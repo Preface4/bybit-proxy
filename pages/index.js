714c1182f100f3e2e70140977fb6fe1100131105
Preface4 <bf12jd0@proton.me>
2025-01-18 18:20:08 +0300

    Add security headers

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
+import Head from 'next/head'
+
 export default function Home() {
   return (
-    <div 
-      dangerouslySetInnerHTML={{ 
-        __html: '<iframe src="/calculator.html" style="width: 100%; height: 100vh; border: none;"></iframe>' 
-      }} 
-    />
+    <>
+      <Head>
+        <meta httpEquiv="Content-Security-Policy" content="frame-ancestors 'self' https://*.google.com https://*.googleusercontent.com" />
+      </Head>
+      <div 
+        dangerouslySetInnerHTML={{ 
+          __html: '<iframe src="/calculator.html" style="width: 100%; height: 100vh; border: none;"></iframe>' 
+        }} 
+      />
+    </>
   );
 }
